a2f9f16bbd12f6e071de4edf21812b211ae0d27e
Alistair Green <alistair.green@alumni-oxford.com>
2018-07-06 20:17:07 +0100

    Minimal ASP.Net MVC application with one static page.

--- MvcAddUserExample/App_Start/RouteConfig.cs
+++ MvcAddUserExample/App_Start/RouteConfig.cs
@@ -13,10 +13,12 @@ namespace MvcAddUserExample
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Users", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
