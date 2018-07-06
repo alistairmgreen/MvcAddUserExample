cc2edd56f3d26d44503556adce6778aa58ae555e
Alistair Green <alistair.green@alumni-oxford.com>
2018-07-06 21:28:49 +0100

    Add unit tests project.

---
+++ MvcAddUserExample.Tests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using FluentAssertions;
+using MvcAddUserExample.Controllers;
+using NUnit.Framework;
+
+namespace MvcAddUserExample.Tests.Controllers
+{
+    [TestFixture]
+    public class UsersControllerTests
+    {
+        private UsersController controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            controller = new UsersController();
+        }
+
+        [Test]
+        public void ShowRegistrationFormReturnsTheRegistrationForm()
+        {
+            ActionResult result = controller.ShowRegistrationForm();
+            result.Should().BeOfType<ViewResult>()
+                .Which.ViewName.Should().Be("RegistrationForm");
+        }
+    }
+}
